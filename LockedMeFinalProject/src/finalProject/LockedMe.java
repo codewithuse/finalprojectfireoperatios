0ed142170fd5de601b2925ad00a4a109b2ff7419
vaibhav <vaibhav@gmail.com>
2022-03-15 21:57:24 +0530

    Display menu

---
+++ LockedMeFinalProject/src/finalProject/LockedMe.java
@@ -0,0 +1,86 @@
+package finalProject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class LockedMe 
+{
+	static final String projectFilesPath = "D:\\Notes";	
+	public static void displayMenu()
+	{
+		System.out.println("****************************");
+		System.out.println("\tWelcome to LockedMe.com Secure App");
+		System.out.println("\tDeveloped by:Vaibhav");
+		System.out.println("****************");
+		System.out.println("\t1. Display All The Files");
+		System.out.println("\t2. Add A New File");
+		System.out.println("\t3. Delete A File");
+		System.out.println("\t4. Search A File");
+		System.out.println("\t5. Exit");
+		System.out.println("****************************");
+
+	}
+	
+	public static void getAllFiles()
+	{
+		File file =  new File(projectFilesPath);
+		File[] listOfFiles =  file.listFiles();
+		if(listOfFiles.length>0)
+		{	
+			
+			System.out.println("Files List is below:\n");
+		for(var l:listOfFiles)
+		{
+			System.out.println(l.getName()); // give the files name
+		}
+		}
+		else
+		{
+			System.out.println("The folder is empty");
+		}
+	}
+	
+	public static void createFiles()
+	{
+		try
+		{
+		Scanner obj = new Scanner(System.in);
+		String fileName;
+		System.out.println("Enter the file name:");
+		fileName =  obj.nextLine();
+		
+		int linesCount;
+		System.out.println("Enter How many lines in a file");
+		linesCount = Integer.parseInt(obj.nextLine());
+		
+		FileWriter fw  =  new FileWriter(projectFilesPath+"\\"+fileName);
+		
+		// Read line by line from User
+		
+		for(int i=1;i<=linesCount;i++)
+		{
+			System.out.println("Enter file line:");
+			fw.write(obj.nextLine()+"\n");
+		}
+		System.out.println("File Created Succesfully");
+		fw.close();
+		}
+		catch(Exception Ex)
+		{
+			
+		}
+		
+		
+		
+	}
+	public static void deleteFiles()
+	{
+		
+	}
+	public static void searchFiles()
+	{
+		
+	}
+}
+
